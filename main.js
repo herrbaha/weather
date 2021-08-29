926ffd7985a965d7f74fbcf0cb70076a6536695e
bahadir <baha_i@hotmail.com>
2021-08-29 15:12:41 +0200

    neue datai erstellt für weather2

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 document.querySelector("form").addEventListener("submit", (e) => {
 e.preventDefault();
 
+let API_KEY = 6ff1093c34249bf39ca11651ec53e460;
+
 let city = e.target.elements.city.value;
 
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=de`;
 
+fetch(url).then((response) =>response.json())
+.then(data => {
+    let cityName = data.name;
+    let temperature = data.main.temp;
+    let humidity = data.main.humidity;
+    let pressure = data.main.pressure;
+    let windSpeed = data.wind.speed;
 
+    let text = `The weather in ${cityName} is ${temperature} <sup>o</sup>C , wind speed is ${windSpeed}, pressure is ${pressure}, and humidity is ${humidity}`;
+    document.querySelector("article").innerHTML = text;
 })
+.catch(error => console.error(error));
+});
