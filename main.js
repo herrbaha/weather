421cf91950159f82e78cb337bfc2acb6f7b087c3
bahadir <baha_i@hotmail.com>
2021-09-02 18:10:04 +0200

    letzte3

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let city = e.target.elements.city.value;
 
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=de`;
 
-fetch(url).then((response) =>response.json())
+fetch(url).then(response =>response.json())
 .then(data => {
     let cityName = data.name;
     let temperature = data.main.temp;
@@ -16,7 +16,7 @@ fetch(url).then((response) =>response.json())
     let windSpeed = data.wind.speed;
 
     let text = `The weather in ${cityName} is ${temperature} <sup>o</sup>C , wind speed is ${windSpeed}, pressure is ${pressure}, and humidity is ${humidity}`;
-    document.querySelector("#weatherContainer").innerHTML = text;
+    document.querySelector("#weatherContainer").innerHTML += text;
 })
 .catch(error => console.error(error));
 });
